7e81bd30dec7130e43e553c225f7e1c45118b9f2
KimTaeYang <explore2012121@gmail.com>
2021-05-06 16:51:15 +0900

    main 이미지 여러개로 나오는 문제 해결

--- src/main/java/com/daelim/transactions/mapper/DaoAttach.java
+++ src/main/java/com/daelim/transactions/mapper/DaoAttach.java
@@ -3,6 +3,7 @@ package com.daelim.transactions.mapper;
 import com.daelim.transactions.dto.AttachDTO;
 import com.daelim.transactions.dto.BoardDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -14,7 +15,11 @@ public interface DaoAttach {
 
     public int selectAttachTotalCount();
 
+    public int selectAttachOneCount(int boardIdx);
+
     public int selectAttachIdxCount(List<BoardDTO> boardList);
 
     public List<AttachDTO> selectAttachListByBoardIdx(List<BoardDTO> boardList);
+
+    public AttachDTO selectAttachOne(@Param("boardIdx")int boardIdx, @Param("count")int count);
 }
